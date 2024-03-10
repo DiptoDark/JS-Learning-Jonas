f3479c8cf7e94dbe7d53d68fea0f188a6dc211bd
Dipto <reazulislamdipto@gmail.com>
2024-03-10 21:49:42 +0600

    Day-1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,62 @@
-let js = "amazing";
+/*let js = "amazing";
 if (js=== "amazing") alert ("JS is the boss");
 
 
 console.log(70-1);
 let firstName = "Dipto";
-console.log(firstName);
+console.log( typeof firstName);
+let year;
+year = 1991; //let chara dile variable er value update hoy//
+let age = 7;
+age = 8;
+const birthDate = 1991;
+const fName= "Dipto Islam";
+const lname = "is";
+console.log(fName + "" + lname);
+
+let x = 10;
+x += 10;
+x++;
+x--;
+console.log(x);
+console.log( fName > lname);
+
+let x, y;
+x = y = 25-19;
+*/
+
+
+
+//Assignment-1//
+let  population;
+
+const country = "Bangladesh";
+const continent = "Asia";
+population = 1400000;
+console.log (country, continent, population);
+
+//Assignment-2//
+let isIsland = true;
+let language;
+console.log (isIsland, country, continent, population);
+language = "Bengali";
+
+//Assignment-3//
+let halfPopulation = population / 2;
+console.log(halfPopulation);
+console.log(halfPopulation++);
+let populationFinland = 6000000;
+console.log (population > populationFinland);
+let avgPopulation = 300000;
+console.log (population < avgPopulation);
+
+let description = "'Portugal is in Europe, and its 11 million people speak portuguese'";
+
+
+
+
+
+
+
+
+
