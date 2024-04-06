bd66a11838ec237d1c7deff27c62b1c221bd3f5f
Dipto6686 <dipto.islam@optimite.net>
2024-04-06 14:25:27 +0600

    Guess the number-Higscore & refactor

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,6 +2,10 @@
 // console.log( document.querySelector('.message').textContent);
 let number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
+const displayMessage =function(message){
+  document.querySelector('.message').textContent = message;
+}
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -12,32 +16,46 @@ document.querySelector('.check').addEventListener('click', function () {
       (document.querySelector('.message').textContent = 'Not a number')
     );
   } else if (guess === number) {
-    document.querySelector('.message').textContent = 'Winner';
+    displayMessage('Winner');
     document.querySelector('.number').textContent = number;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-  } else if (guess > number) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'too high';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'Lost';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
-  } else if (guess < number) {
+  } else if (guess !== number){
     if (score > 1) {
-      document.querySelector('.message').textContent = 'too low';
+      displayMessage(guess > number ? 'too high' : 'too low');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'Lost';
+      displayMessage('Lost');
+      document.querySelector('.score').textContent = 0;
     }
   }
+  // else if (guess > number) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = 'too high';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = 'Lost';
+  //   }
+  // } else if (guess < number) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = 'too low';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = 'Lost';
+  //   }
+  // }
 });
 document.querySelector('.again').addEventListener('click', function () {
   number = Math.trunc(Math.random() * 20) + 1;
   score = 20;
-  document.querySelector('.message').textContent = 'Start guessing';
+  displayMessage('Start guessing');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
