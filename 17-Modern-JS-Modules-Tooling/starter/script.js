a8ce5a5b02f8023f27aa9a92400103aa4b59b3b3
diptodark <reazulislamdipto@gmail.com>
2024-06-21 19:53:27 +0600

    Bohut kisu

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,8 @@
+//Importing Module
+// import { addToCart } from './shoppingCart.js';
+
+// console.log('Importing module');
+// addToCart('milk', 5);
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('Milk', 4);
